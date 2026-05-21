9f78186d90add8fe3cca4495cbd0fea3406bda0f
Mercerenies <mercerenies@gmail.com>
2026-05-21 19:33:56 -0400

    Initial commit

---
+++ lib/rake/tree.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "tree/version"
+
+module Rake
+  module Tree
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/rake/tree/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Rake
+  module Tree
+    VERSION = "0.1.0"
+  end
+end
